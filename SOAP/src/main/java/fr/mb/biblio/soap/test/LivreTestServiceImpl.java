68d1d11b81338e5e268776ff209570bda1a9dd4c
morgan <brighi.morgan@wanadoo.fr>
2018-05-17 23:17:15 +0200

    Modification configuration Spring pour autoriser les annotations fonctionnelle

--- SOAP/src/main/java/fr/mb/biblio/soap/test/LivreTestServiceImpl.java
+++ SOAP/src/main/java/fr/mb/biblio/soap/test/LivreTestServiceImpl.java
@@ -4,6 +4,7 @@ package fr.mb.biblio.soap.test;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.mb.biblio.dao.impl.UtilisateurHome;
