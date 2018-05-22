95d7e64f43763779cdd7b3aadb115122b89b24b8
morgan <brighi.morgan@wanadoo.fr>
2018-05-22 08:57:32 +0200

    possibilité de rajout d'un utilisateur + obtention d'un utilisateur by id dans le web service

--- SOAP/src/main/java/fr/mb/biblio/soap/userService/contract/userService.java
+++ SOAP/src/main/java/fr/mb/biblio/soap/userService/contract/userService.java
@@ -5,15 +5,50 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import fr.mb.biblio.models.beans.Utilisateur;
+import fr.mb.biblio.models.exception.FunctionalException;
 import fr.mb.biblio.models.exception.NotFoundException;
 
+/**
+ * Service de gestion de l'utilisateur
+ * @author Morgan
+ *
+ */
 @WebService(name="userService")
 public interface userService {
 	
+	/**
+	 * Méthode d'identification de l'utilisateur
+	 * @param identifiant
+	 * @param mdp
+	 * @return
+	 * @throws NotFoundException
+	 */
 	@WebMethod
 	public Utilisateur identification(
 			@WebParam(name="identifiant")String identifiant,
 			@WebParam(name="mdp")String mdp)
 				throws NotFoundException;
+	
+	/**
+	 * Méthode de rajout d'un utilisateur
+	 * @param utilisateur
+	 * @throws FunctionalException
+	 */
+	@WebMethod
+	public void insert(
+			@WebParam(name="utilisateur")Utilisateur utilisateur)
+				throws FunctionalException;
+	
+	/**
+	 * Méthode d'obtention d'un utilisateur via son id
+	 * @param id
+	 * @return
+	 * @throws NotFoundException
+	 */
+	@WebMethod
+	public Utilisateur getUtilisateurById(
+			@WebParam(name="id")Integer id)
+				throws NotFoundException;
 
 }
+
--- SOAP/src/main/java/fr/mb/biblio/soap/userService/impl/userServiceImpl.java
+++ SOAP/src/main/java/fr/mb/biblio/soap/userService/impl/userServiceImpl.java
@@ -7,28 +7,75 @@ import fr.mb.biblio.dao.contract.UtilisateurDao;
 import fr.mb.biblio.models.beans.Utilisateur;
 import fr.mb.biblio.models.exception.NotFoundException;
 import fr.mb.biblio.soap.userService.contract.userService;
+import fr.mb.biblio.models.exception.FunctionalException;
 
+/**
+ * Service de gestion d'un utilisateur
+ * 
+ * @author Morgan
+ *
+ */
 public class userServiceImpl implements userService {
 
+	/**
+	 * DAO à injecter
+	 */
 	@Inject
 	UtilisateurDao utilisateurDao;
-	
+
+	/**
+	 * Utilisé pour la récupération d'un utilisateur en bd
+	 */
 	Utilisateur user;
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.mb.biblio.soap.userService.contract.userService#identification(java.lang.
+	 * String, java.lang.String)
+	 */
 	@Override
 	@Transactional
-	public Utilisateur identification(String identifiant, String mdp) throws NotFoundException{
+	public Utilisateur identification(String identifiant, String mdp) throws NotFoundException {
+
+		user = utilisateurDao.identification(identifiant, mdp);
 		
-		
-		user=utilisateurDao.identification(identifiant, mdp);
-		if (user==null) {
+		if (user == null) {
 			throw new NotFoundException("Identifiant ou Mot de passe incorrect");
 		}
-			
-		
+
 		return user;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.mb.biblio.soap.userService.contract.userService#insert(fr.mb.biblio.models
+	 * .beans.Utilisateur)
+	 */
+	@Override
+	@Transactional
+	public void insert(Utilisateur utilisateur) throws FunctionalException {
+		if (utilisateur == null)
+			throw new FunctionalException("Utilisateur null");
+		else
+			utilisateurDao.persist(utilisateur);
+
+	}
+
+	@Override
+	@Transactional
+	public Utilisateur getUtilisateurById(Integer id) throws NotFoundException {
+			user = utilisateurDao.findById(id);
 		
-		
+		if (user == null) {
+			throw new NotFoundException("Utilisateur non trouvé");
+		}
+
+		return user;
 	}
 
 }
