3da181f02f1b3e540fd87a928f36b61fffba724d
Morgan <brighi.morgan@wanadoo.fr>
2018-11-16 15:05:06 +0100

    changement profile spring pour test

--- dao/src/test-dao/java/fr/mb/biblio/dao/testDao/ITResaDaoImpl.java
+++ dao/src/test-dao/java/fr/mb/biblio/dao/testDao/ITResaDaoImpl.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
     @ExtendWith(SpringExtension.class)
     @ContextConfiguration(locations= {"/ApplicationContext.xml"})
-    @ActiveProfiles(profiles = "dev")
+    @ActiveProfiles(profiles = "test")
     @Transactional
     public class ITResaDaoImpl {
 
