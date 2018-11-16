1261ebc2fd5221fadfd747de750367aff732ff3e
Morgan <brighi.morgan@wanadoo.fr>
2018-11-16 15:03:23 +0100

    Configuration Failsafe Plugin

---
+++ dao/src/test-dao/java/fr/mb/biblio/dao/testDao/ITResaDaoImpl.java
@@ -0,0 +1,39 @@
+package fr.mb.biblio.dao.testDao;
+
+import fr.mb.biblio.dao.contract.ResaDao;
+import fr.mb.biblio.models.beans.Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+    @ExtendWith(SpringExtension.class)
+    @ContextConfiguration(locations= {"/ApplicationContext.xml"})
+    @ActiveProfiles(profiles = "dev")
+    @Transactional
+    public class ITResaDaoImpl {
+
+        @Autowired
+        ResaDao resaDao;
+
+        @Test
+        public void getResaByUserId() {
+            List<Reservation> resa = resaDao.getResaByUserId(2);
+        }
+
+        @Test
+        public void init(){
+            assertNotNull(resaDao,"OK");
+        }
+    }
+
