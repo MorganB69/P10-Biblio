3d97442a5fb594b85253072076e6770606447eab
Morgan <brighi.morgan@wanadoo.fr>
2018-11-23 18:09:30 +0100

    Rajout d'un test d'intégration service Resa

---
+++ SOAP/src/test-soap/java/fr/mb/biblio/test/IT/ITResaServiceImpl.java
@@ -0,0 +1,26 @@
+package fr.mb.biblio.test.IT;
+
+import fr.mb.biblio.models.beans.Reservation;
+import fr.mb.biblio.models.exception.FunctionalException;
+import fr.mb.biblio.models.exception.NotFoundException;
+import fr.mb.biblio.soap.resaService.contract.ResaService;
+import fr.mb.biblio.soap.resaService.impl.ResaServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/ApplicationContext.xml")
+public class ITResaServiceImpl {
+
+    ResaService resaService = new ResaServiceImpl();
+
+    @Test
+    public void newReservationTest() throws NotFoundException, FunctionalException {
+        Reservation resa = resaService.newReservation(8,2);
+        assertTrue(resa.getId()!=0);
+
+    }
+}
